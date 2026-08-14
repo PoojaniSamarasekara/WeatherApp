28508cb5a3bed3e78b8440e29d7a84b8d7b3d205
Poojani Samarasekara <poojanisamarasekara16@gmail.com>
2026-08-14 06:54:31 +0530

    Configure Android app for REST API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,8 @@ android {
 }
 
 dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
